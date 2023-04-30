0305e218eafcada8bcfdd116f0ac7609b9529379
Ola <hollabodey.kilaso@gmail.com>
2023-04-30 23:34:20 +0100

    Completed Design

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
 let emailInput = document.getElementById('emailInput');
 let button = document.querySelector('.btn');
-let error = document.querySelector('.err');
+let error = document.querySelector('.errorDiv');
 
 button.addEventListener('click', handleSubmit);
 
 function handleSubmit(e){
     e.preventDefault();
-    
+
     let validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+
+    // console.log(emailInput.value)
 
     if(emailInput.value.match(validRegex)){
         alert("Valid email address!");
         console.log('error');
         return true;
-    }else {
-        error.innerText = "Please enter a valid email";
+    } else {
+        error.style.display = 'block';
+        error.innerHTML = "Please enter a valid email";
     }
 
 
